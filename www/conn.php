9beb79ac8e93d93ecad128ca13e6b652df729c52
brayn003 <rudraprasaddas@outlook.com>
2015-12-26 13:12:01 +0530

    Removed important credentials

--- www/conn.php
+++ www/conn.php
@@ -1,12 +1,8 @@
 <?php
-//include 'home_admin.html';
-	$hostname="localhost";  // Hostname
-	// $username="teknajvh_root";       // Username for Database
-	// $password="Meradata#1#1";           // Database password
-	// $dbname="teknajvh_teknack15";        // Database name
-	$username="root";       // Username for Database
-	$password="";           // Database password
-	$dbname="teknack_promo";        // Database name
+	$hostname="xxx";  
+	$username="xxx";   
+	$password="xxx";      
+	$dbname="xxx";      
 	
 	$con = mysql_connect($hostname,$username,$password) or die("Error connecting to database");  // Make Connection	
 	mysql_select_db($dbname)                     // Select Database
