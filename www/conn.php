ecba75d77ca8eca4786fa5c49ae1dd181362ba10
brayn003 <rudraprasaddas@outlook.com>
2015-12-30 18:58:05 +0530

    Removed conn configuration file

--- www/conn.php
+++ www/conn.php
@@ -1,8 +1,8 @@
 <?php
-	$hostname="xxx";  
-	$username="xxx";   
-	$password="xxx";      
-	$dbname="xxx";      
+	$hostname="localhost";  
+	$username="root";   
+	$password="";      
+	$dbname="teknack_promo";      
 	
 	$con = mysql_connect($hostname,$username,$password) or die("Error connecting to database");  // Make Connection	
 	mysql_select_db($dbname)                     // Select Database
