164df73e0e6e60fc5f405c3ba89272a1276d1f96
brayn003 <rudraprasaddas@outlook.com>
2015-12-30 19:04:48 +0530

    Removed connection using gitignore

--- www/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$hostname="localhost";  
-	$username="root";   
-	$password="";      
-	$dbname="teknack_promo";      
-	
-	$con = mysql_connect($hostname,$username,$password) or die("Error connecting to database");  // Make Connection	
-	mysql_select_db($dbname)                     // Select Database
-?>
